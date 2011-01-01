968f74bf94e6d17febafcd80228249e0b11a456a
Vince Vincent <mellterm@gmail.com>
2011-01-01 11:29:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mellterm11::Application.config.secret_token = '293bc9f4db60ac13e3b329d7097aa5ee933fa1b33cdca18308f5dc67dd77b9e0052312d6a56e4136a9ba8207a40026539f69e35877a0bd8b365c6dd33d5ec262'
