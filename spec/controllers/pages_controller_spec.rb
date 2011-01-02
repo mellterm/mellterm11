38db234f36d73ba8318f33f5673ee91150dc1f10
Vince Vincent <mellterm@gmail.com>
2011-01-02 12:33:13 +0000

    Added title helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
 render_views
 
+before(:each) do
+	@base_title = "Mellterm Basic Job Man Application"
+end
+
 describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -13,7 +17,7 @@ describe "GET 'home'" do
   	it "should have the right title" do
 		get 'home'
 		response.should have_selector("title",
-		:content => "Ruby on Rails Mellterm | Home")
+		:content => "#{@base_title} | Home")
 	end
   
 	it "should have a non-blank body" do
@@ -32,7 +36,7 @@ describe "GET 'contact'" do
   	it "should have the right title" do
 		get 'contact'
 		response.should have_selector("title",
-		:content => "Ruby on Rails Mellterm | Contact")
+		:content => "#{@base_title} | Contact")
 	end
 end
 
@@ -45,7 +49,7 @@ describe "GET 'about'" do
 	it "should have the right title" do
 		get 'about'
 		response.should have_selector("title",
-		:content => "Ruby on Rails Mellterm | About")
+		:content => "#{@base_title} | About")
 	end
 end
 
