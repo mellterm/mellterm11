9f68bd1adab8fe4197f78de0c5568e84fdbc30f8
Vince Vincent <mellterm@gmail.com>
2011-01-03 18:02:06 +0000

    added password and encrypted_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,24 @@
 # == Schema Information
-# Schema version: 20110103082404
+# Schema version: 20110103175526
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  phone      :string(255)
-#  skype      :string(255)
-#  time_zone  :string(255)
-#  notes      :text
-#  rate       :integer
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  phone              :string(255)
+#  skype              :string(255)
+#  time_zone          :string(255)
+#  notes              :text
+#  rate               :integer
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email,:phone, :skype, :time_zone, :notes, :rate 
+	attr_accessor	:password
+	attr_accessible :name, :email,:phone, :skype, :time_zone, :notes, :rate, :password, :password_confirmation 
 	
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[1-z]+\z/i
 	phone_regex = /\A[\d+\-\s]+\z/
@@ -31,4 +33,9 @@ class User < ActiveRecord::Base
 	validates :phone, 	:presence => true,
 						:length => {:minimum => 4, :maximum => 15},
 						:format => {:with => phone_regex}
+						
+	validates :password, 	:presence => true,
+							:confirmation => true,
+							:length => {:within => 6..10}
+						
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,20 @@ require 'spec_helper'
 describe User do
 	
 	before(:each) do
-@attr = { 	:name => "Example User", 
-		   	:email => "user@example.com", 
-		   	:phone => "023-476-292",
-		   	:skype => "blankskype",
-		   	:time_zone => "UTC+2",
-		   	:notes => "instructions",
-		   	:rate => 0.08
-	   	}
+	@attr = { 	:name => "Example User", 
+			   	:email => "user@example.com", 
+			   	:phone => "023-476-292",
+			   	:skype => "blankskype",
+			   	:time_zone => "UTC+2",
+			   	:notes => "instructions",
+			   	:rate => 0.08,
+			   	:password =>"foobar",
+			   	:password_confirmation =>"foobar"
+	   		}
 	end
 	
 	
-  it "should create a new instance given a valid attribute" do
+ 	it "should create a new instance given a valid attribute" do
   	User.create!(@attr)
   	end
   	
@@ -41,13 +43,14 @@ describe User do
 		valid_email_user = User.new(@attr.merge(:email => address))
 		valid_email_user.should be_valid
 	end
-end
+	end
+	
 	it 'should reject invalid email addresses' do
 		addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
 		addresses.each do |address|
 		invalid_email_user = User.new(@attr.merge(:email => address))
 		invalid_email_user.should_not be_valid
-		end
+	end
 	end
 	
 	it 'should reject invalid phone numbers' do
@@ -55,7 +58,7 @@ end
 		numbers.each do |number|
 		invalid_phone_user = User.new(@attr.merge(:phone => number))
 		invalid_phone_user.should_not be_valid		
-		end
+	end
 	end
 	
 		it 'should reject phone numbers that are too long' do
@@ -70,5 +73,56 @@ end
 	user_with_duplicate_email = User.new(@attr)
 	user_with_duplicate_email.should_not be_valid
 	end
+	
+	describe "passwords" do
+		
+		before(:each) do
+			@user = User.new(@attr)
+		end
+		
+		it "should have a password attribute" do
+			@user.should respond_to(:password)
+		end
+	
+		it "should have a password confirmation attribute" do
+			@user.should respond_to(:password_confirmation)
+		end
+	end
+		
+	describe "password validations"	do
+		
+		it "should require a password" do
+		User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+		end
+	
+		it "should require a matching password confirmation" do
+			User.new(@attr.merge(:password_confirmation => "invalid_password")).
+			should_not be_valid
+		end
+		
+		it "should reject short passwords" do
+			short = "a"*5
+			hash = @attr.merge(:password => short, :password_confirmation => short)
+			User.new(hash).should_not be_valid
+		end
 			
+		it "should reject long passwords" do
+			long = "a"*11
+			hash = @attr.merge(:password => long, :password_confirmation => long)
+			User.new(hash).should_not be_valid
+		end
+	end
+	
+	describe "password encryption" do
+		
+		before(:each) do
+			@user = User.create!(@attr)
+		end 
+		
+		it "should have an encrypted password attribute" do
+		@user.should respond_to(:encrypted_password)
+		end		
+	
+	end
+	
 end
