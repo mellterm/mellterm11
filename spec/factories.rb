395c5cf60a54cdc40f04a1385a4e63a878675ac4
Vince Vincent <mellterm@gmail.com>
2011-01-04 14:24:46 +0000

    added a User factory and test for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+Factory.define :user do |user|
+	user.name					"Vince Vincent"
+	user.phone					"123344"
+	user.email					"Vince@vincent.com"
+	user.password				"foobar"
+	user.password_confirmation	"foobar"
+	user.skype					"vvv"
+end
